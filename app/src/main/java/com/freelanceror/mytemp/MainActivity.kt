8df53a5bb43b9bd90557e2ff8e701e27e80d7b0a
Wesley de Oliveira <wesleysistemaor@gmail.com>
2025-03-12 18:53:25 -0300

    Correçcao de bugs

--- app/src/main/java/com/freelanceror/mytemp/MainActivity.kt
+++ app/src/main/java/com/freelanceror/mytemp/MainActivity.kt
@@ -3,29 +3,18 @@ package com.freelanceror.mytemp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.freelanceror.mytemp.ui.feature.WeatherRoute
 import com.freelanceror.mytemp.ui.theme.MyTempTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
-MyTempTheme {
-
-       WeatherRoute()
-
+            MyTempTheme {
+                WeatherRoute()
             }
-
-
         }
     }
 }
